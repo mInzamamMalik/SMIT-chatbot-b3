fb9be3e7b2be75d2040ad0ac9bee0e09daf910e7
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-10 14:34:55 +0500

    process.env added for hosting

--- 05. express server/01. hello world/server.mjs
+++ 05. express server/01. hello world/server.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:", req);
