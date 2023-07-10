981d2931fd051203c9b993cb7080d9b8cc904e03
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-10 11:19:00 +0500

    class code app

---
+++ 05. express server/01. hello world/server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:", req);
+  res.send({ message: "Hello World! by Shehzad" });
+});
+
+app.get("/name/:myName", (req, res) => {
+  const name = req.params.myName;
+
+  console.log("🚀 ~ file: server.mjs:13 ~ app.get ~ name:", name);
+
+  res.send({ message: `Hello World! by ${name}` });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
