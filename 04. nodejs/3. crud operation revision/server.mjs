c9f46d201109e4ba7f90124d96c0e00117e97c8a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-19 13:51:34 +0500

    revision

--- 04. nodejs/3. crud operation revision/server.mjs
+++ 04. nodejs/3. crud operation revision/server.mjs
@@ -1,15 +1,16 @@
 import express from "express";
 import { customAlphabet } from "nanoid";
-const nanoid = customAlphabet("1234567890", 20);
 
+const nanoid = customAlphabet("1234567890", 20);
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-// console.log("testing nanoid : ",nanoid());
+console.log("testing nanoid : ", nanoid());
 
 const products = [
   {
@@ -83,15 +84,15 @@ app.post("/product", (req, res) => {
         price: "$23.12",
         description: "abc product description"
       }`);
-      return;
+    return;
   }
- 
-  console.log("testing");
-  res.send("validation pass");
-  return;
+
+  // console.log("testing");
+  // res.send("validation pass");
+  // return;
 
   products.push({
-    id: nanoid(),
+    id: "17",
     name: req.body.name,
     price: req.body.price,
     description: req.body.description,
@@ -100,69 +101,69 @@ app.post("/product", (req, res) => {
   res.status(201).send({ message: "created product" });
 });
 
-app.put("/product/:id", (req, res) => {
-  if (!req.body.name && !req.body.price && !req.body.description) {
-    res.status(403).send(`
-      required parameter missing. 
-      atleast one parameter is required: name, price or description to complete update
-      example JSON request body:
-      {
-        name: "abc product",
-        price: "$23.12",
-        description: "abc product description"
-      }`);
-  }
+// app.put("/product/:id", (req, res) => {
+//   if (!req.body.name && !req.body.price && !req.body.description) {
+//     res.status(403).send(`
+//       required parameter missing.
+//       atleast one parameter is required: name, price or description to complete update
+//       example JSON request body:
+//       {
+//         name: "abc product",
+//         price: "$23.12",
+//         description: "abc product description"
+//       }`);
+//   }
 
-  let isFound = false;
+//   let isFound = false;
 
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === req.params.id) {
-      isFound = i;
-      break;
-    }
-  }
+//   for (let i = 0; i < products.length; i++) {
+//     if (products[i].id === req.params.id) {
+//       isFound = i;
+//       break;
+//     }
+//   }
 
-  if (isFound === false) {
-    res.status(404);
-    res.send({
-      message: "product not found",
-    });
-  } else {
-    if (req.body.name) products[isFound].name = req.body.name;
-    if (req.body.price) products[isFound].price = req.body.price;
-    if (req.body.description)
-      products[isFound].description = req.body.description;
+//   if (isFound === false) {
+//     res.status(404);
+//     res.send({
+//       message: "product not found",
+//     });
+//   } else {
+//     if (req.body.name) products[isFound].name = req.body.name;
+//     if (req.body.price) products[isFound].price = req.body.price;
+//     if (req.body.description)
+//       products[isFound].description = req.body.description;
 
-    res.send({
-      message: "product is updated with id: " + products[isFound].id,
-      data: products[isFound],
-    });
-  }
-});
+//     res.send({
+//       message: "product is updated with id: " + products[isFound].id,
+//       data: products[isFound],
+//     });
+//   }
+// });
 
-app.delete("/product/:id", (req, res) => {
-  let isFound = false;
+// app.delete("/product/:id", (req, res) => {
+//   let isFound = false;
 
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === req.params.id) {
-      isFound = i;
-      break;
-    }
-  }
+//   for (let i = 0; i < products.length; i++) {
+//     if (products[i].id === req.params.id) {
+//       isFound = i;
+//       break;
+//     }
+//   }
 
-  if (isFound === false) {
-    res.status(404);
-    res.send({
-      message: "product not found",
-    });
-  } else {
-    products.splice(isFound, 1);
+//   if (isFound === false) {
+//     res.status(404);
+//     res.send({
+//       message: "product not found",
+//     });
+//   } else {
+//     products.splice(isFound, 1);
 
-    res.send({
-      message: "product is deleted",
-    });
-  }
-});
+//     res.send({
+//       message: "product is deleted",
+//     });
+//   }
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
