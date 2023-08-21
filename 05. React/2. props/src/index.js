3b5de47b7a4576f7ef09168d03a4613e3ceaa0a6
M Inzamam Malik <malikasinger1@gmail.com>
2023-08-21 11:06:42 +0500

    class code

--- 05. React/2. props/src/index.js
+++ 05. React/2. props/src/index.js
@@ -21,26 +21,71 @@ function Hi(props) {
   );
 }
 
-function Post(props) {
-  return <div className="facebookPost">
-    <h3>{props.name}</h3>
+const Post = ({ name, body, imgUrl }) => (
+  <div className="facebookPost">
+    <h3>{name}</h3>
     <span>3 July 2023</span>
-    <p>Here's a cool thing about props: you can pass whatever you want into them. You’re not restricted to strings, or trying to guess what it will do with your string (cough Angular1). Remember earlier, and 30 seconds ago, how we put a JS expression inside single braces? Well, you can do the same thing with a prop’s value:</p>
+    <p>{body}</p>
 
-    <img width={300} src={props.imgUrl}></img>
+    {
+      (imgUrl) ? <img width={300} src={imgUrl} alt="this is poster image"></img> : null
+    }
+
+    {/* {imgUrl && <img width={300} src={imgUrl} alt="this is poster image"></img>} */}
+
+
+
     <br />
     <button>Like</button>
     <button>Comment</button>
     <button>share</button>
-
   </div>
-}
+)
 
+// const Gate = ({ isOpen }) => (
+//   <div>gate is {(isOpen) ? "open" : "closed"}</div>
+// )
+// ReactDOM.render(<Gate isOpen={true} />, document.getElementById('root'))
 
 
 ReactDOM.render(<div>
-  <Post imgUrl={"https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/homepage/families-gallery/2023/revuelto/revuelto_m.png"} text={"jhsdgfljskdhflksdjhf"} name={"John"}/>
-  <Post imgUrl={"https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200"} text={"jhsdgfljskdhflksdjhf"} name={"Malik"}/>
-  <Post imgUrl={"https://static.pakwheels.com/2022/09/Ferrari-Purosangue-revealed-11-scaled.jpg"} text={"jhsdgfljskdhflksdjhf"} name={"Malik"}/>
-  <Post imgUrl={"https://i.tribune.com.pk/media/images/7UOKRCYY7NJTFHXVVO5XFZLCK41641535289-0/7UOKRCYY7NJTFHXVVO5XFZLCK41641535289-0.jpg"} text={"jhsdgfljskdhflksdjhf"} name={"Malik"}/>
+  <Post
+    text={"jhsdgfljskdhflksdjhf"}
+    name={"John"}
+    body={<b>Here's a cool thing about props: you can pass whatever you want into them. You’re not restricted to strings, or trying to guess what it will do with your string (cough Angular1). Remember earlier, and 30 seconds ago, how we put a JS expression inside single braces? Well, you can do the same thing with a prop’s value:</b>} />
+  <Post
+    imgUrl={"https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200"}
+    text={"jhsdgfljskdhflksdjhf"}
+    name={"Malik"}
+    body={"Here's a cool thing about props: you can pass whatever you want into them. You’re not restricted to strings, or trying to guess what it will do with your string (cough Angular1). Remember earlier, and 30 seconds ago, how we put a JS expression inside single braces? Well, you can do the same thing with a prop’s value:"} />
+  <Post
+    imgUrl={"https://static.pakwheels.com/2022/09/Ferrari-Purosangue-revealed-11-scaled.jpg"}
+    text={"jhsdgfljskdhflksdjhf"}
+    name={"Malik"}
+    body={"Here's a cool thing about props: you can pass whatever you want into them. You’re not restricted to strings, or trying to guess what it will do with your string (cough Angular1). Remember earlier, and 30 seconds ago, how we put a JS expression inside single braces? Well, you can do the same thing with a prop’s value:"} />
+  <Post
+    imgUrl={"https://i.tribune.com.pk/media/images/7UOKRCYY7NJTFHXVVO5XFZLCK41641535289-0/7UOKRCYY7NJTFHXVVO5XFZLCK41641535289-0.jpg"} text={"jhsdgfljskdhflksdjhf"} name={"Malik"} />
 </div>, document.querySelector("#root"));
+
+
+
+
+let Card = ({ firstName }) => <div>hello {firstName}</div>
+
+
+
+// let obj = { name: "john", roll: "abc" };
+
+// // let name = obj.name
+// // let roll = obj.roll
+
+// let { name, roll } = obj
+
+// let arr = ["John", "alice", "fred"];
+
+// // let one = arr[0];
+// // let two = arr[1];
+// // let last = arr[2];
+
+// let [one, two, last] = arr
+
