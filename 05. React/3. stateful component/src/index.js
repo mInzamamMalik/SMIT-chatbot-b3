a1cf6653d30c1aff69e35ec4d8a428f0341db75a
M Inzamam Malik <malikasinger1@gmail.com>
2023-08-25 10:23:47 +0500

    state

--- 05. React/3. stateful component/src/index.js
+++ 05. React/3. stateful component/src/index.js
@@ -6,15 +6,54 @@ import "./index.css";
 const Home = () => {
 
   const [isLit, setLit] = useState(true);
+  const [temp, setTemp] = useState(72);
 
+  const sub = () => {
+
+    // setTemp(temp - 1); // 72
+
+    setTemp((prev) => {
+      // my logic here
+      return prev - 1;
+    })
+    setTemp((prev) => {
+      // my logic here
+      return prev - 1;
+    })
+
+    console.log("sub: ", temp);
+  }
+  const add = () => {
+    // setTemp(temp + 1);
+    setTemp((prev) => {
+      // my logic here
+      return prev + 1;
+    })
+  }
 
   return <div className={`room ${(isLit) ? "lit" : "dark"}`}>
+
+    <p>
+
+      <button onClick={sub}>-</button>
+      {temp}
+      <button onClick={add}>+</button>
+
+    </p>
+
+
+    <br />
+    <br />
 
     <p> Light is {(isLit) ? "On" : "Off"} </p>
 
     <button onClick={() => { setLit(!isLit) }}>
       Turn {(isLit) ? "off" : "on"}
     </button>
+
+
+
+
   </div>
 }
 
