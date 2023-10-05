d0686fcdcaee4151f3496d0693561af8dc1970a1
M Inzamam Malik <malikasinger1@gmail.com>
2023-10-05 19:31:53 +0500

    class code

--- 05. React/7. mongodb CRUD and vector search/server.mjs
+++ 05. React/7. mongodb CRUD and vector search/server.mjs
@@ -49,6 +49,31 @@ app.use(cors(["http://localhost:3000", "127.0.0.1", "https://ewrer234234.appspot
 
 app.use(morgan('combined'));
 
+app.use("/webhook", (req, res, next) => {
+  console.log("req.body", req.body);
+
+  const body = {
+    detectIntentResponseId: '4dd48764-d86c-4d2c-a319-26447fa40e95',
+    intentInfo: {
+      lastMatchedIntent: 'projects/chatbotdemo-mykk/locations/us-central1/agents/0ceea8d4-84ab-407a-af7f-d01cd135841d/intents/c6585558-9cd8-458a-91a6-09081c27a309',
+      displayName: 'confirmation.yes',
+      confidence: 1
+    },
+    pageInfo: {
+      currentPage: 'projects/chatbotdemo-mykk/locations/us-central1/agents/0ceea8d4-84ab-407a-af7f-d01cd135841d/flows/00000000-0000-0000-0000-000000000000/pages/a059d196-a603-41ba-bc0c-f4cd67602fb8',
+      formInfo: {},
+      displayName: 'Booking Confirmation Page'
+    },
+    sessionInfo: {
+      session: 'projects/chatbotdemo-mykk/locations/us-central1/agents/0ceea8d4-84ab-407a-af7f-d01cd135841d/sessions/ec1130-477-ead-eb0-cf69558fb',
+      parameters: { guestname: [Object], numberofguests: 2, roomtype: 'business' }
+    },
+    fulfillmentInfo: { tag: 'abc' },
+    text: 'yes',
+    languageCode: 'en'
+  }
+})
+
 
 app.get("/api/v1/stories", async (req, res) => {
   const cursor = postCollection
